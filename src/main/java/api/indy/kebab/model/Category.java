cab34032c88de161b2f4e99294c9a13b986ed63a
Marcin <160152512+Gallus-Anonimus@users.noreply.github.com>
2025-10-12 22:37:08 +0200

    Add Category Enittity and repository for JPA integration (#5)

---
+++ src/main/java/api/indy/kebab/model/Category.java
@@ -0,0 +1,60 @@
+package api.indy.kebab.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Access(AccessType.PROPERTY)
+@Table(name = "categories")
+public class Category {
+    private long _categoryId;
+    private String _name;
+    private String _iconUrl;
+    private String _description;
+
+    public Category() {}
+
+    public Category(long categoryId, String name, String iconUrl, String description) {
+        this._categoryId = categoryId;
+        this._name = name;
+        this._iconUrl = iconUrl;
+        this._description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    public long getCategoryId() {
+        return this._categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this._categoryId = categoryId;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return this._name;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+
+    @Column(name = "icon_url", nullable = false)
+    public String getIconUrl() {
+        return this._iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this._iconUrl = iconUrl;
+    }
+
+    @Column(name = "description",nullable = false)
+    public String getDescription() {
+        return this._description;
+    }
+
+    public void setDescription(String description) {
+        this._description = description;
+    }
+}
