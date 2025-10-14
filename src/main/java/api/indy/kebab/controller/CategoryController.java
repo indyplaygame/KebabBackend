9102cdca8db2433338207eccad349658634fffb5
indyPlayGames <marcinekkowalczynek@gmail.com>
2025-10-14 15:31:21 +0200

    Update JPA DDL auto setting

--- src/main/java/api/indy/kebab/controller/CategoryController.java
+++ src/main/java/api/indy/kebab/controller/CategoryController.java
@@ -8,7 +8,6 @@ import api.indy.kebab.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
