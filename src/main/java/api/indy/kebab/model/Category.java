812d3ec0e826a845d3103a88970d3b81be7e9a06
indyPlayGames <marcinekkowalczynek@gmail.com>
2025-10-14 23:35:55 +0200

    Refactor entity models

--- src/main/java/api/indy/kebab/model/Category.java
+++ src/main/java/api/indy/kebab/model/Category.java
@@ -15,7 +15,7 @@ public class Category {
     private String _iconUrl;
     private String _description;
 
-    public Category() {}
+    protected Category() {}
 
     public Category(String name, String iconUrl, String description) {
         this._name = name;
@@ -23,81 +23,21 @@ public class Category {
         this._description = description;
     }
 
-    /**
-     * Gets the category ID.
-     *
-     * @return Category ID.
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "categoryId", nullable = false)
-    public long getCategoryId() {
-        return this._categoryId;
-    }
+    public long getCategoryId() { return this._categoryId; }
+    protected void setCategoryId(long categoryId) { this._categoryId = categoryId; }
 
-    /**
-     * Sets the category ID.
-     *
-     * @param categoryId Category ID.
-     */
-    public void setCategoryId(long categoryId) {
-        this._categoryId = categoryId;
-    }
-
-    /**
-     * Gets the name of the category.
-     *
-     * @return Category name.
-     */
     @Column(name = "name", nullable = false, length = 30)
-    public String getName() {
-        return this._name;
-    }
+    public String getName() { return this._name; }
+    public void setName(String name) { this._name = name; }
 
-    /**
-     * Sets the name of the category.
-     *
-     * @param name Category name.
-     */
-    public void setName(String name) {
-        this._name = name;
-    }
-
-    /**
-     * Gets the icon URL of the category.
-     *
-     * @return Icon URL.
-     */
     @Column(name = "iconUrl", nullable = false)
-    public String getIconUrl() {
-        return this._iconUrl;
-    }
+    public String getIconUrl() { return this._iconUrl;}
+    public void setIconUrl(String iconUrl) { this._iconUrl = iconUrl; }
 
-    /**
-     * Sets the icon URL of the category.
-     *
-     * @param iconUrl Icon URL.
-     */
-    public void setIconUrl(String iconUrl) {
-        this._iconUrl = iconUrl;
-    }
-
-    /**
-     * Gets the description of the category.
-     *
-     * @return Category description.
-     */
     @Column(name = "description", nullable = true, length = 1000)
-    public String getDescription() {
-        return this._description;
-    }
-
-    /**
-     * Sets the description of the category.
-     *
-     * @param description Category description.
-     */
-    public void setDescription(String description) {
-        this._description = description;
-    }
+    public String getDescription() { return this._description; }
+    public void setDescription(String description) { this._description = description; }
 }
--- src/main/java/api/indy/kebab/model/User.java
+++ src/main/java/api/indy/kebab/model/User.java
@@ -19,7 +19,7 @@ public class User {
     private String _passwordHash;
     private String _dateOfBirth;
 
-    public User() {}
+    protected User() {}
 
     public User(String username, String email, String firstName, String middleName, String lastName, String passwordHash, String dateOfBirth) {
         this._username = username;
@@ -31,157 +31,37 @@ public class User {
         this._dateOfBirth = dateOfBirth;
     }
 
-    /**
-     * Gets the user ID.
-     *
-     * @return User ID.
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userId", nullable = false)
-    public long getUserId() {
-        return this._userId;
-    }
+    public long getUserId() { return this._userId; }
+    protected void setUserId(long userId) { this._userId = userId; }
 
-    /**
-     * Sets the user ID.
-     *
-     * @param userId User ID.
-     */
-    public void setUserId(long userId) {
-        this._userId = userId;
-    }
-
-    /**
-     * Gets the username of the user.
-     *
-     * @return Username.
-     */
     @Column(name = "username", nullable = false, unique = true, length = 20)
-    public String getUsername() {
-        return this._username;
-    }
+    public String getUsername() { return this._username; }
+    public void setUsername(String username) { this._username = username; }
 
-    /**
-     * Sets the username of the user.
-     *
-     * @param username Username.
-     */
-    public void setUsername(String username) {
-        this._username = username;
-    }
-
-    /**
-     * Gets the email address of the user.
-     *
-     * @return Email address.
-     */
     @Column(name = "email", nullable = false, unique = true, length = 100)
-    public String getEmail() {
-        return this._email;
-    }
+    public String getEmail() { return this._email; }
+    public void setEmail(String email) { this._email = email; }
 
-    /**
-     * Sets the email address of the user.
-     *
-     * @param email Email address.
-     */
-    public void setEmail(String email) {
-        this._email = email;
-    }
-
-    /**
-     * Gets the first name of the user.
-     *
-     * @return First name.
-     */
     @Column(name = "firstName", nullable = false, length = 50)
-    public String getFirstName() {
-        return this._firstName;
-    }
+    public String getFirstName() { return this._firstName; }
+    public void setFirstName(String firstName) { this._firstName = firstName; }
 
-    /**
-     * Sets the first name of the user.
-     *
-     * @param firstName First name.
-     */
-    public void setFirstName(String firstName) {
-        this._firstName = firstName;
-    }
-
-    /**
-     * Gets the middle name of the user.
-     *
-     * @return Middle name.
-     */
     @Column(name = "middleName", nullable = true, length = 50)
-    public String getMiddleName() {
-        return this._middleName;
-    }
+    public String getMiddleName() { return this._middleName; }
+    public void setMiddleName(String middleName) { this._middleName = middleName; }
 
-    /**
-     * Sets the middle name of the user.
-     *
-     * @param middleName Middle name.
-     */
-    public void setMiddleName(String middleName) {
-        this._middleName = middleName;
-    }
-
-    /**
-     * Gets the last name of the user.
-     *
-     * @return Last name.
-     */
     @Column(name = "lastName", nullable = false, length = 50)
-    public String getLastName() {
-        return this._lastName;
-    }
+    public String getLastName() { return this._lastName; }
+    public void setLastName(String lastName) { this._lastName = lastName; }
 
-    /**
-     * Sets the last name of the user.
-     *
-     * @param lastName Last name.
-     */
-    public void setLastName(String lastName) {
-        this._lastName = lastName;
-    }
-
-    /**
-     * Gets the hashed password of the user.
-     *
-     * @return Hashed password.
-     */
     @Column(name = "passwordHash", nullable = false, length = 200)
-    public String getPasswordHash() {
-        return this._passwordHash;
-    }
+    public String getPasswordHash() { return this._passwordHash; }
+    public void setPasswordHash(String passwordHash) { this._passwordHash = passwordHash; }
 
-    /**
-     * Sets the hashed password of the user.
-     *
-     * @param passwordHash Hashed password.
-     */
-    public void setPasswordHash(String passwordHash) {
-        this._passwordHash = passwordHash;
-    }
-
-    /**
-     * Gets the date of birth of the user.
-     *
-     * @return Date of birth in the format YYYY-MM-DD.
-     */
     @Column(name = "dateOfBirth", nullable = false, length = 10)
-    public String getDateOfBirth() {
-        return this._dateOfBirth;
-    }
-
-    /**
-     * Sets the date of birth of the user.
-     *
-     * @param dateOfBirth Date of birth in the format YYYY-MM-DD.
-     */
-    public void setDateOfBirth(String dateOfBirth) {
-        this._dateOfBirth = dateOfBirth;
-    }
+    public String getDateOfBirth() { return this._dateOfBirth; }
+    public void setDateOfBirth(String dateOfBirth) { this._dateOfBirth = dateOfBirth; }
 }
