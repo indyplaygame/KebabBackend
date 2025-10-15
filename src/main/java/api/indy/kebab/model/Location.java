0b6bc616f9133cb6a96ca1f6fca30e52bb211b34
Marcin <160152512+Gallus-Anonimus@users.noreply.github.com>
2025-10-15 22:46:50 +0200

    Added Location Model and Voivodeship enum (#16)
    Co-authored-by: indyPlayGames <marcinekkowalczynek@gmail.com>

---
+++ src/main/java/api/indy/kebab/model/Location.java
@@ -0,0 +1,73 @@
+package api.indy.kebab.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Embeddable
+public class Location  {
+    private double _latitude;
+    private double _longitude;
+    private String _country;
+    private Voivodeship _voivodeship;
+    private String _postalCode;
+    private String _city;
+    private String _street;
+    private String _buildingNumber;
+
+    protected Location() {}
+
+    public Location(
+        double latitude,
+        double longitude,
+        String country,
+        Voivodeship voivodeship,
+        String postalCode,
+        String city,
+        String street,
+        String buildingNumber
+    ) {
+        this._latitude = latitude;
+        this._longitude = longitude;
+        this._country = country;
+        this._voivodeship = voivodeship;
+        this._postalCode = postalCode;
+        this._city = city;
+        this._street = street;
+        this._buildingNumber = buildingNumber;
+    }
+
+    @Column(name = "latitude", nullable = true)
+    public Double getLatitude() { return _latitude; }
+    public void setLatitude(double latitude) { this._latitude = latitude; }
+
+    @Column(name = "longitude", nullable = true)
+    public Double getLongitude() { return this._longitude; }
+    public void setLongitude(double longitude) { this._longitude = longitude; }
+
+    @Column(name = "country", nullable = false, columnDefinition = "varchar(50) default 'Poland'")
+    public String getCountry() { return this._country; }
+    public void setCountry(String country) { this._country = country; }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "voivodeship", nullable = true)
+    public Voivodeship getVoivodeship() { return this._voivodeship; }
+    public void setVoivodeship(Voivodeship voivodeship) { this._voivodeship = voivodeship; }
+
+    @Column(name = "postalCode", nullable = false, length = 6)
+    public String getPostalCode() { return this._postalCode; }
+    public void setPostalCode(String postalCode) { this._postalCode = postalCode; }
+
+    @Column(name = "city", nullable = false, length = 50)
+    public String getCity() { return this._city; }
+    public void setCity(String city) { this._city = city; }
+
+    @Column(name = "street", nullable = false, length = 50)
+    public String getStreet() { return this._street; }
+    public void setStreet(String street) { this._street = street; }
+
+    @Column(name = "buildingNumber", nullable = false, length = 50)
+    public String getBuildingNumber() { return this._buildingNumber; }
+    public void setBuildingNumber(String buildingNumber) { this._buildingNumber = buildingNumber; }
+}
---
+++ src/main/java/api/indy/kebab/model/Voivodeship.java
@@ -0,0 +1,21 @@
+package api.indy.kebab.model;
+
+@SuppressWarnings("SpellCheckingInspection")
+public enum Voivodeship {
+    DOLNOSLASKIE,
+    KUJAWSKO_POMORSKIE,
+    LUBELSKIE,
+    LUBUSKIE,
+    LODZKIE,
+    MALOPOLSKIE,
+    MAZOWIECKIE,
+    OPOLSKIE,
+    PODKARPACKIE,
+    PODLASKIE,
+    POMORSKIE,
+    SLASKIE,
+    SWIETOKRZYSKIE,
+    WARMIENSKO_MAZURSKIE,
+    WIELKOPOLSKIE,
+    ZACHODNIOPOMORSKIE
+}
