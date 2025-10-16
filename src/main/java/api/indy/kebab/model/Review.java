10a4c9f8369d648a9c7c5c00edfa91760e55bc05
indyy_ <38891612+indyplaygame@users.noreply.github.com>
2025-10-16 17:27:35 +0200

    Refactor Review model

--- src/main/java/api/indy/kebab/model/Review.java
+++ src/main/java/api/indy/kebab/model/Review.java
@@ -1,6 +1,8 @@
 package api.indy.kebab.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -15,43 +17,38 @@ import jakarta.validation.constraints.Min;
 @SuppressWarnings("AssociationNotMarkedInspection")
 public class Review {
     private long _reviewId;
-    private long _userId;
-    private long _orderId;
-    private float _rating=0.0f;
     private String _title;
     private String _description;
     private String _imageUrl;
-    private boolean _anonymous;
-    private long _likes=0;
     private String _createdAt;
     private String _updatedAt;
+    private User _user;
+    private boolean _anonymous;
+    private float _rating;
+    private long _likes;
 
     protected Review() {}
 
     public Review(
-        long reviewId,
-        long userId,
-        long orderId,
-        float rating,
         String title,
         String description,
         String imageUrl,
-        boolean anonymous,
-        long likes,
         String createdAt,
-        String updatedAt
-    ) {
-        this._reviewId = reviewId;
-        this._userId = userId;
-        this._orderId = orderId;
-        this._rating = rating;
+        String updatedAt,
+        User user,
+        boolean anonymous,
+        float rating,
+        long likes
+        ) {
         this._title = title;
         this._description = description;
         this._imageUrl = imageUrl;
-        this._anonymous = anonymous;
-        this._likes = likes;
         this._createdAt = createdAt;
         this._updatedAt = updatedAt;
+        this._user = user;
+        this._anonymous = anonymous;
+        this._rating = rating;
+        this._likes = likes;
     }
 
 
@@ -61,21 +58,7 @@ public class Review {
     public long getReviewId() { return this._reviewId; }
     protected void setReviewId(long reviewId) { this._reviewId = reviewId; }
 
-    @Column(name="userId", nullable = false)
-    public long getUserId() { return this._userId; }
-    protected void setUserId(long userId) { this._userId = userId; }
-
-    @Column(name="orderId", nullable = false)
-    public long getOrderId() { return this._orderId; }
-    protected void setOrderId(long orderId) { this._orderId = orderId; }
-
-    @Min(0)
-    @Max(5)
-    @Column(name = "rating", nullable = false)
-    public float getRating() { return this._rating; }
-    public void setRating(float rating) { this._rating = rating; }
-
-    @Column(name="title",nullable = true ,length = 100)
+    @Column(name="title", nullable = true ,length = 100)
     public String getTitle() { return this._title; }
     public void setTitle(String title) { this._title = title; }
 
@@ -83,25 +66,39 @@ public class Review {
     public String getDescription() { return this._description; }
     public void setDescription(String description) { this._description = description; }
 
-    @Column(name = "imageUrl",nullable = true)
+    @Column(name = "imageUrl", nullable = true)
     public String getImageUrl() { return this._imageUrl; }
     public void setImageUrl(String imageUrl) { this._imageUrl = imageUrl; }
 
-    @Column(name = "anonymous",nullable = false)
-    public boolean isAnonymous() { return this._anonymous; }
-    public void setAnonymous(boolean anonymous) { this._anonymous = anonymous; }
-
-    @Column(name = "likes",nullable = false)
-    public long getLikes() { return this._likes; }
-    public void setLikes(long likes) { this._likes = likes; }
-
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "createdAt", nullable = false)
     public String getCreatedAt() { return this._createdAt; }
     public void setCreatedAt(String createdAt) { this._createdAt = createdAt; }
 
-    @Column(name = "updated_at")
+    @Column(name = "updatedAt", nullable = true)
     public String getUpdatedAt() { return this._updatedAt; }
     public void setUpdatedAt(String updatedAt) { this._updatedAt = updatedAt; }
 
+    @JsonIgnore
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "userId", nullable = true)
+    public User getUser() { return this._user; }
+    public void setUser(User user) { this._user = user; }
 
+    @Column(name = "anonymous", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    public boolean isAnonymous() { return this._anonymous; }
+    public void setAnonymous(boolean anonymous) { this._anonymous = anonymous; }
+
+    @Min(0)
+    @Max(5)
+    @Column(name = "rating", nullable = false, columnDefinition = "FLOAT DEFAULT 0")
+    public float getRating() { return this._rating; }
+    public void setRating(float rating) { this._rating = rating; }
+
+    @Column(name = "likes", nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    public long getLikes() { return this._likes; }
+    public void setLikes(long likes) { this._likes = likes; }
+
+    @Transient
+    @JsonProperty
+    public long getUserId() { return this._user != null ? this._user.getUserId() : 0; }
 }
