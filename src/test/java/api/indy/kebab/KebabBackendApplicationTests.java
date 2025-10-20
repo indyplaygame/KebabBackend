6a1727079035287b54ec35433244026e242fe319
indyy_ <38891612+indyplaygame@users.noreply.github.com>
2025-10-20 10:38:37 +0200

    Reorder gradle plugins

---
+++ src/test/java/api/indy/kebab/KebabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.indy.kebab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KebabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
