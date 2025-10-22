8d8223dd5e48333f9d30c4fa2e5252af2597c792
indyy_ <38891612+indyplaygame@users.noreply.github.com>
2025-10-22 21:44:56 +0200

    Add health controller, fix issue #33 (#35)

--- src/main/java/api/indy/kebab/config/WebConfig.java
+++ src/main/java/api/indy/kebab/config/WebConfig.java
@@ -3,6 +3,7 @@ package api.indy.kebab.config;
 import api.indy.kebab.auth.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(this._authInterceptor);
+    }
+
+    /**
+     * Configures Cross-Origin Resource Sharing (CORS) settings.
+     *
+     * <p>This method is called by the Spring framework to define CORS mappings
+     * for the application, allowing cross-origin requests from specified origins.</p>
+     *
+     * @param registry the {@link CorsRegistry} used to configure CORS mappings.
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("*")
+            .allowedOrigins("*", "http://localhost:*", "http://localhost")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
